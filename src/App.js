60bc2b212762344b96ee8c02c22b5d0641b03170
Roronoa786Zoro <aniket199mistry@gmail.com>
2023-07-02 11:10:43 +0530

    Redux app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './component/NavBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Shop from './Shop'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <NavBar/>
+    <div className='container mt-5'>
+      <Routes>
+        <Route exact path='/' element={<Shop/>}/>
+      </Routes>
     </div>
-  );
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className=''>
+        <h2 className=''>Buy Shoes For: 50 Rs</h2>
+        <div>
+            <button className='btn btn-primary mx-2'>-</button> Add this item to cart. 
+            <button className='btn btn-primary mx-2'>+</button>
+        </div>
+    </div>
+  )
+}
+
+export default Shop
