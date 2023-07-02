59f1b37587bd9aec8953421efb62d5c068a67227
Roronoa786Zoro <aniket199mistry@gmail.com>
2023-07-02 11:39:12 +0530

    Day 2

--- src/Shop.js
+++ src/Shop.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Shop = () => {
   return (
     <div className=''>
-        <h2 className=''>Buy Shoes For: 50 Rs</h2>
+        <h2 className=''>Deposit/Withdraw Money</h2>
         <div>
-            <button className='btn btn-primary mx-2'>-</button> Add this item to cart. 
+            <button className='btn btn-primary mx-2'>-</button> Update Balance 
             <button className='btn btn-primary mx-2'>+</button>
         </div>
     </div>
